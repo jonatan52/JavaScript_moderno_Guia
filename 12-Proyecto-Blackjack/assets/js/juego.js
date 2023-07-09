fe756b846094fd9ec5421c8a61e728d0617441bd
jonatan52 <tarriba52@gmail.com>
2023-07-08 20:55:28 -0500

    añadiendo funciones al proyecto

--- 12-Proyecto-Blackjack/assets/js/juego.js
+++ 12-Proyecto-Blackjack/assets/js/juego.js
@@ -7,16 +7,39 @@
 
 let deck = [];
 const tipos = ['C','D','H','S'];
+const especiales = ['A','J','Q','K'];
 
 const crearDeck = () =>{
     for(let i=2; i<=10; i++){
         for(let tipo of tipos){
-            deck.push(i + tipo)
+            deck.push(i + tipo);
         }
 
     }
-    console.log(deck)
+     
+    for(let tipo of tipos){
+        for(let esp of especiales){
+            deck.push(esp + tipo)
+        }
+
+    }
+
+    // console.log(deck);
+    deck = _.shuffle(deck);
+    console.log(deck);
+    return deck;
 }
 
 crearDeck();
+
+// esta funcion me permite tomar una nueva carta
+const pedirCarta = () =>{
+    if (deck.length === 0){
+        throw 'No hay cartas en el deck';
+    }
+    const carta = deck.pop;
+
+    return carta;
+
+} 
 
